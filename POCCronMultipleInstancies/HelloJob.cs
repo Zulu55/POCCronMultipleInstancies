1976f7de10ebe2f13f2eaf91acbf82d315b8d551
Juan Carlos Zuluaga <jzuluaga@lwolf.com>
2023-11-14 12:23:02 -0500

    Basic problem reproduction

---
+++ POCCronMultipleInstancies/HelloJob.cs
@@ -0,0 +1,13 @@
+﻿using Quartz;
+
+namespace POCCronMultipleInstancies
+{
+    public class HelloJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            await Task.Delay(1000);
+            Console.WriteLine($"[{context.JobDetail.ToString()!.Substring(11, 73)}] RUN");
+        }
+    }
+}
