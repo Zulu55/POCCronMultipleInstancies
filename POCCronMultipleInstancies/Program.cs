9a99d2e83049cf0fd463b7837bfd92468c603a2d
Juan Carlos Zuluaga <jzuluaga@lwolf.com>
2023-11-16 12:58:51 -0500

    Begin

--- POCCronMultipleInstancies/Program.cs
+++ POCCronMultipleInstancies/Program.cs
@@ -33,15 +33,34 @@ internal class Program
 public class RecurringTaskWorkflow : IWorkflow
 {
     private readonly IClock _clock;
+    private readonly ILockService _lockService; // Servicio de bloqueo personalizado
 
     private readonly Guid _process = Guid.NewGuid();
 
-    public RecurringTaskWorkflow(IClock clock) => _clock = clock;
+    public RecurringTaskWorkflow(IClock clock, ILockService lockService)
+    {
+        _clock = clock;
+        _lockService = lockService;
+    }
 
     public string Process => _process.ToString().Substring(30, 6);
 
     public void Build(IWorkflowBuilder builder) =>
         builder
             .Timer(Duration.FromSeconds(1))
-            .WriteLine(() => $"Process [{Process}] Time [{_clock.GetCurrentInstant()}]");
+            .Then(() => TryExecuteProcess());
+
+    private void TryExecuteProcess()
+    {
+        if (_lockService.TryAcquireLock(Process))
+        {
+            Console.WriteLine($"Process [{Process}] Time [{_clock.GetCurrentInstant()}]");
+            // Ejecutar la lógica del proceso aquí
+            _lockService.ReleaseLock(Process);
+        }
+        else
+        {
+            Console.WriteLine("El proceso ya está siendo ejecutado por otra instancia.");
+        }
+    }
 }
