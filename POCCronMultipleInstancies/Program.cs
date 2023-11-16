2e184c73e96ebcd39ca81c8149408ff33c071511
Juan Carlos Zuluaga <jzuluaga@lwolf.com>
2023-11-16 12:41:06 -0500

    In the same file

--- POCCronMultipleInstancies/Program.cs
+++ POCCronMultipleInstancies/Program.cs
@@ -1,22 +1,47 @@
 ﻿using Elsa;
+using Elsa.Activities.Console;
+using Elsa.Activities.Temporal;
+using Elsa.Builders;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using POCCronMultipleInstancies;
+using NodaTime;
 
-var host = new HostBuilder()
-    .ConfigureServices((hostContext, services) =>
-    {
-        services
-    .AddElsa(elsa => elsa
-        .AddConsoleActivities()
-        .AddQuartzTemporalActivities()
-        .AddWorkflow<RecurringTaskWorkflow>());
-    })
-    .UseConsoleLifetime()
-    .Build();
-
-using (host)
+internal class Program
 {
-    await host.StartAsync();
-    await host.WaitForShutdownAsync();
+    private static async Task Main(string[] args)
+    {
+        var host = new HostBuilder()
+            .ConfigureServices((hostContext, services) =>
+            {
+                services
+            .AddElsa(elsa => elsa
+                .AddConsoleActivities()
+                .AddQuartzTemporalActivities()
+                .AddWorkflow<RecurringTaskWorkflow>());
+            })
+            .UseConsoleLifetime()
+            .Build();
+
+        using (host)
+        {
+            await host.StartAsync();
+            await host.WaitForShutdownAsync();
+        }
+    }
+}
+
+public class RecurringTaskWorkflow : IWorkflow
+{
+    private readonly IClock _clock;
+
+    private readonly Guid _process = Guid.NewGuid();
+
+    public RecurringTaskWorkflow(IClock clock) => _clock = clock;
+
+    public string Process => _process.ToString().Substring(30, 6);
+
+    public void Build(IWorkflowBuilder builder) =>
+        builder
+            .Timer(Duration.FromSeconds(1))
+            .WriteLine(() => $"Process [{Process}] Time [{_clock.GetCurrentInstant()}]");
 }
