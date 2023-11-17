8d23d5bcec333de8a20492b633ac82bd65b2f652
Juan Carlos Zuluaga <jzuluaga@lwolf.com>
2023-11-17 14:56:06 -0500

    xxx

--- POCCronMultipleInstancies/Program.cs
+++ POCCronMultipleInstancies/Program.cs
@@ -2,6 +2,7 @@
 using Elsa.Activities.Console;
 using Elsa.Activities.Temporal;
 using Elsa.Builders;
+using Elsa.Extensions;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using NodaTime;
@@ -10,14 +11,19 @@ internal class Program
 {
     private static async Task Main(string[] args)
     {
+        var dbConnectionString = "Data Source=.;Initial Catalog=POC;User ID=sa;Password=Roger1974.;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        var redisConnectionString = "zulutest.redis.cache.windows.net:6380,password=OWuJzbESK5HFAE5jMPLxnVk1s9oxUKYQkAzCaAqTIao=,ssl=True,abortConnect=False";
+
         var host = new HostBuilder()
             .ConfigureServices((hostContext, services) =>
             {
-                services
-            .AddElsa(elsa => elsa
-                .AddConsoleActivities()
-                .AddQuartzTemporalActivities()
-                .AddWorkflow<RecurringTaskWorkflow>());
+                services.AddRedis(redisConnectionString);
+                services.AddElsa(elsa => elsa
+                        .AddConsoleActivities()
+                        .AddQuartzTemporalActivities()
+                        .AddWorkflow<RecurringTaskWorkflow>()
+                        .ConfigureDistributedLockProvider(options => options.UseSqlServerLockProvider(dbConnectionString))
+                        .UseRedisCacheSignal());
             })
             .UseConsoleLifetime()
             .Build();
